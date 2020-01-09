0ca5b185039bbc0dbb2833ad90815b4828be49b4
liuning11 <liuning11@jd.com>
2020-01-09 18:34:49 +0800

    fixed ingrore

--- woaidu_crawler/woaidu_crawler/commands/init_single_mongodb.py
+++ woaidu_crawler/woaidu_crawler/commands/init_single_mongodb.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-#-*-coding:utf-8-*-
+# -*-coding:utf-8-*-
 
 """
     This file is for initiate mongodb situation
@@ -36,30 +36,34 @@ DATABASE_NAME = "books_fs"
 client = None
 DATABASE_HOST = "localhost"
 DATABASE_PORT = 27017
-INDEX = {\
-            #collection
-            'book_detail':\
-                {\
-                    (('book_name',ASCENDING),('author',ASCENDING)):{'name':'book_name_author','unique':True},
-                    'book_name':{'name':'book_name'},
-                    'author':{'name':'author'},
-                    'alias_name':{'name':'alias_name'},
-                }\
+INDEX = {
+    # collection
+    'book_detail':
+        {
+            (('book_name', ASCENDING), ('author', ASCENDING)): {'name': 'book_name_author', 'unique': True},
+            'book_name': {'name': 'book_name'},
+            'author': {'name': 'author'},
+            'alias_name': {'name': 'alias_name'},
         }
+}
+
 
 def drop_database(name_or_database):
     if name_or_database and client:
         client.drop_database(name_or_database)
 
+
 def create_index():
     """
         create index for books_fs.book_detail
     """
-    for k,v in INDEX.items():
-        for key,kwargs in v.items():
-            client[DATABASE_NAME][k].ensure_index(list(key) if type(key)==types.TupleType else key,**kwargs)
+    for k, v in INDEX.items():
+        for key, kwargs in v.items():
+            client[DATABASE_NAME][k].ensure_index(list(key) if type(key) == isinstance(types.TupleType) else key,
+                                                  **kwargs)
+
 
 if __name__ == "__main__":
-    client = MongoClient(DATABASE_HOST,DATABASE_PORT) 
+    client = MongoClient(DATABASE_HOST, DATABASE_PORT)
     drop_database(DATABASE_NAME)
-    create_index() 
+    create_index()
